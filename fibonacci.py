53ef3a49f2b2c3840842b1c19ddb32c8bd742903
tmoriya <moriya.takayasu514@gmail.com>
2018-04-06 23:46:10 +0900

    add triple step problem

---
+++ fibonacci.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
+def triple_step(n):
+    """Compute the number of cases of triple steps."""
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        a = 1
+        b = 2
+        c = 4
+        for i in range(3, n):
+            d = a + b + c
+            a = b
+            b = c
+            c = d
+        return c
+
+
+if __name__ == '__main__':
+    N = int(input())
+    print(triple_step(N))
