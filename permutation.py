7421d92f161617fcc4ff9d4217c2a270db70a03c
tmoriya <moriya.takayasu514@gmail.com>
2018-04-09 23:52:39 +0900

    add permutate string

--- permutation.py
+++ permutation.py
@@ -25,6 +25,27 @@ def permutation_of_two_iterabele(iterable1, iterable2, r=None):
             yield bst_lis
 
 
+def permutate_string(string):
+    len_s = len(string)
+    if len_s == 1:
+        return [string]
+    else:
+        result = []
+        c = string[0]
+        d = string[1:]
+        for i in range(len_s):
+            if i == 0:
+                for st in permutate_string(d):
+                    result.append(c + st)
+            elif i == len_s - 1:
+                for st in permutate_string(d):
+                    result.append(st + c)
+            else:
+                for st in permutate_string(d):
+                    result.append(st[0:i] + c + st[i:])
+        return result
+
+
 if __name__ == '__main__':
     lis = list(permutation_of_two_iterabele([4, 2, 5, 1], [8, 7, 9]))
     for li in lis:
@@ -34,3 +55,4 @@ if __name__ == '__main__':
     for li in lis:
         print(li)
     print(len(lis))
+    print(permutate_string('abcd'))
